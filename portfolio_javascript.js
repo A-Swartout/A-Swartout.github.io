e160f566fd29eec1e1160e5152765dd99eea601f
Austin Swartout <pictureofperception@gmail.com>
2020-06-01 17:29:35 -0400

    update portfolio

---
+++ portfolio_javascript.js
@@ -0,0 +1,24 @@
+// Smooth scrolling effect to links
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if(this.Link !== "") {
+            event.preventDefault();
+            var Link = this.Link;
+            $('html, body').animate({
+                scrollTop: $(Link).offset().top
+            }, 2000, function(){
+                window.location.Link = Link;
+            });
+        }
+    });
+});
+
+// Open and close contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    this.Close();
+}
